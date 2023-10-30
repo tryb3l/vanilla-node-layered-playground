56d22a6ddad5b4ea639dff6ab09a037ca8c7220e
tryb3l <56679619+tryb3l@users.noreply.github.com>
2023-10-30 22:57:46 +0100

    updated handler

--- src/handler.mjs
+++ src/handler.mjs
@@ -1,34 +1,35 @@
 "use strict";
 import { parse } from "node:url";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import { DEFAULT_HEADERS } from "./utils/util.mjs";
+import { routes } from "./routes/userRoute.mjs";
+import { generateInstance } from "./factories/userFactory.mjs";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+const filePath = join(currentDir, "/database", "data.json");
+const userService = generateInstance({ filePath });
+
+const userRoutes = routes({ userService: {} });
 
 const allRoutes = {
-  "/users:get": (request, response) => {
-    response.writeHead(200, DEFAULT_HEADERS);
-    response.end("Hello World");
-  },
-  //404 route
-  default: (request, response) => {
-    response.writeHead(404, DEFAULT_HEADERS);
-    response.write("ooops, not found");
-    response.end();
-  },
+  ...userRoutes,
 };
 
 function handler(request, response) {
   const { url, method } = request;
   const { pathname } = parse(url, true);
-  console.log("url:", url);
-  console.log("method:", method);
   const key = `${pathname}:${method.toLowerCase()}`;
   const choosen = allRoutes[key] || allRoutes.default;
+
   return Promise.resolve(choosen(request, response)).catch(
     errorHandler(response),
   );
 }
 
-function errorHandler(response) {
-  return (error) => {
+function errorHandler(response, request) {
+  return (/** @type {{ stack: any; message: string; }} */ error) => {
     console.log("error:", error.stack);
     response.writeHead(500, DEFAULT_HEADERS);
     response.write(
